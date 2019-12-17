39878ccb0d6abc94e753cbbc15b61b5e9b0431bc
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2019-12-17 01:08:15 -0500

    Added meteor functionality and particle effects

---
+++ Assets/Scripts/ImpactEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactEvent : MonoBehaviour {
+  [Tooltip("Expects a particle effect")]
+  public GameObject explosion;
+  public int damage = 1;
+
+  void OnCollisionEnter2D(Collision2D other){
+    gameObject.GetComponent<Rotate>().enabled = false;
+
+    if (explosion){
+      Instantiate(explosion,transform.position,transform.rotation);
+    }
+
+    // if(other.tag == "player"){
+    //  other.gameObject.GetComponent<CharacterController>().killCar(damage);
+    //}
+
+    // if(other.tag == "planet"){
+    //  other.gameObject.GetComponent<CharacterController>().AddScore();
+    //}
+
+    other.gameObject.GetComponent<AudioSource>().Play();
+
+    DestroyObject(this.gameObject);
+  }
+}
