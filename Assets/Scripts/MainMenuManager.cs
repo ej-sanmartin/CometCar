aa0b0e39a46c1755ba72c71eccd1112f29e70e67
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2020-01-05 04:39:50 -0500

    set up the UI for car selection panel. Now need to be able to instantiate chosen car

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -13,9 +13,25 @@ public class MainMenuManager : MonoBehaviour {
   // references to Button GameObjects
 	public Button startButton;
 	public Button carsButton;
+  public Button backButton;
 
   // list the selectable cars
-	public GameObject[] carNames;
+	public Button[] carNames;
+
+  // UI for best score and a reference to the best score int
+  // also references needed scores to unlock cars
+  public Text UIBestScore;
+  public Text UIBestScore_Outline;
+
+  private int bestScore;
+  [HideInInspector]
+  public Text UIScoreRequirement;
+  [HideInInspector]
+  public int UIScoreRequirement_ToCompare;
+  [HideInInspector]
+  public Text UIScoreRequirement_Outline;
+  [HideInInspector]
+  public int UIScoreRequirement_Outline_ToCompare;
 
   // references FadeOutEvent
   public GameObject _FadeOut;
@@ -28,19 +44,44 @@ public class MainMenuManager : MonoBehaviour {
     // setup the listener to loadlevel when clicked
     startButton.onClick.RemoveAllListeners();
     startButton.onClick.AddListener(() => StartGame());
+
+    // setup the listener to loadlevel when clicked
+    carsButton.onClick.RemoveAllListeners();
+    carsButton.onClick.AddListener(() => ShowMenu("CarSelect"));
+
+    // setup the listener to loadlevel when clicked
+    backButton.onClick.RemoveAllListeners();
+    backButton.onClick.AddListener(() => ShowMenu("Main"));
   }
 
   void setCarSelect(){
     _CarsSelectMenu.SetActive(true);
+
+    bestScore = PlayerPrefManager.GetHighscore();
+
+    UIBestScore.text = bestScore.ToString();
+    UIBestScore_Outline.text = bestScore.ToString();
 
     // loops through each car defined in the editor
     // TODO: make car buttons interactable if player reaches needed highscore and unlocked them
     //       if not locked, disable and make car black
     //       also make the selected car instantiate into game scene.
     for(int i = 0; i < carNames.Length; i++){
-      GameObject carName = carNames[i];
+      Button carCard = carNames[i];
+      UIScoreRequirement = carCard.transform.FindDeepChild("ScoreNeededToUnlock").GetComponent<Text>();
+      UIScoreRequirement_ToCompare = int.Parse(UIScoreRequirement.text);
 
+      UIScoreRequirement_Outline = carCard.transform.FindDeepChild("ScoreNeededToUnlock_Outline").GetComponent<Text>();
+      UIScoreRequirement_Outline_ToCompare = int.Parse(UIScoreRequirement_Outline.text);
 
+      if(bestScore >= UIScoreRequirement_ToCompare){
+        UIScoreRequirement.enabled = false;
+        UIScoreRequirement_Outline.enabled = false;
+      } else {
+        carCard.GetComponent<Button>().interactable = false;
+        carCard.GetComponent<Image>().color = Color.black;
+
+      }
     }
   }
 
@@ -54,7 +95,7 @@ public class MainMenuManager : MonoBehaviour {
       case "Main":
         _MainMenu.SetActive(true);
         break;
-      case "Cars Select":
+      case "CarSelect":
         _CarsSelectMenu.SetActive(true);
         break;
     }
